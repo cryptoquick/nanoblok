a6fd713173ee135835006937c718a950b2d2fd29
Alex Trujillo <cryptoquick@gmail.com>
2009-07-02 12:37:58 -0600

    Added a jquery loader bar. It doesn't work very well yet. Also, working on block code as well.

--- nb03/nanoblok.js
+++ nb03/nanoblok.js
@@ -1,13 +1,12 @@
 /*
  * Nanoblok (Experimental) - Web-Based Graphical Editor for Game Sprite Development
  *
- * Copyright (c) 2009 Alex Trujillo (http://superluminon.com)
+ * Copyright (c) 2009 Alex Trujillo
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
  */
  
 // Universal constants
 var size = 35;
-//var angle = 45;
 var scale = {x: 1, y: 1};
 var grid = {r: 16, c: 16};
 
@@ -19,30 +18,52 @@ function Init () {
 	// Helps to know how fast things are going -- TEMP
 	var init0 = new Date();
 	
+	$("#progress").progressbar({ value: 50 });
+	
+	// Initialize the nano element as a Raphael canvas
 	var nano = Raphael("nano", 800, 650);
 	
+	// Can't remember if this is necessary...
 	var charCode;
 	
-//	buildSquare(size, axis, angle, scale, trans, nano)
-//	buildSquare(size, 'z', 45, {x: 1, y: 0.5}, {x: 100, y: 200}, nano);
+	// x is a vertical rotation, y is a horizontal rotation,
+	// and z rotates around the screen. Trust me, it makes sense.
+	var angle = {x: 45, y: 30, z: -30};
 	
-//	grid, angle, scale, trans, nano
-	var isogrid = gridMatrix(grid, {x: Math.asin(Math.tan(degRads(30))), y: degRads(45), z: degRads(0)}, scale, {x: 400, y: 200}, nano);
+	//	grid, angle, scale, trans, nano
+	var isogrid = gridMatrix(grid, angle, scale, {x: 0, y: 300}, nano);
 	
 	var init1 = new Date();
 	
 	debug('Program initialized in ' + (init1 - init0) + ' milliseconds.');
 	
-	testInput(isogrid, nano);
+	testInput(isogrid, angle, nano);
+	
+//	drawBlock({x: 150, y: 200}, nano);
 }
 
+/* Utility Functions */
 function debug(input) {
 	var debug = document.getElementById('debug');
 	debug.innerHTML = debug.innerHTML + '<br>' + input;
 }
 
+// Uses a fraction made by dividing the step 
+function progress(step, base) {
+	
+	var percentage = Math.floor((step / base) * 100);
+	
+//	$(function(percentage) {
+		//getter
+	//	var value = $('#progress').progressbar('option', 'value');
+		//setter
+		$('#progress').progressbar('option', 'value', percentage);
+//	});
+
+}
+
 // Function not being used right now, but might be useful later.
-function testInput(isogrid, nano) {
+function testInput(isogrid, angle, nano) {
 	window.addEventListener("keydown", function(evt) {
 		// Input Handling
 		if (evt.type == "keydown") {
@@ -57,45 +78,100 @@ function testInput(isogrid, nano) {
 		// Left arrow key
 		if (charCode == 37) {
 			removeGrid (isogrid);
-			angle++;
-			isogrid = gridMatrix(grid, angle, scale, {x: 400, y: 100}, nano);
+			angle.y++;
+			isogrid = gridMatrix(grid, {y: angle.y, x: 45, z: -30}, scale, {x: 0, y: 300}, nano);;
 		}
 		// Right arrow key
 		if (charCode == 39) {
 			removeGrid (isogrid);
-			angle--;
-			isogrid = gridMatrix(grid, angle, scale, {x: 400, y: 100}, nano);
+			angle.y--;
+			isogrid = gridMatrix(grid, {y: angle.y, x: 45, z: -30}, scale, {x: 0, y: 300}, nano);;
 		}
 	}, false);
 //	return isogrid;
 }
 
-/* Grid Functions */
+/* Block Functions */
+function drawBlock (position, nano) {
+	var angle2 = {x: 0, y: 0, z: 0};
+	
+	var angle = angleBlock(angle2);
+	
+//	buildSquare(size, angle, scale, trans, nano);
+	buildSquare(35, angle, {x: 1, y: 1}, {x: position.x, y: position.y}, nano);
+}
 
-/*function changePerspective (isogrid, perspective, nano) {
+function angleBlock (angle2) {
+	var angle = new Object();
+	angle.x = degRads(angle2.x);
+	angle.y = degRads(angle2.y);
+//	angle.z = Math.atan(0.5);
+	angle.z = degRads(angle2.z);
+	
+/*	angle.x = degRads(45);
+	angle.y = Math.asin(Math.tan(degRads(30)));
+	angle.z = degRads(-30); */
+	
+	return angle;
+}
+
+function drawBlok (posX, posY, scale, nano) {
+	coorset = hexiso(posX, posY, scale);
+	hexset = [1, 2, 3, 4, 5, 6];
+	var blokTop = drawrmb(1, 2, 7, 6, coorset, nano);
+	var blokRight = drawrmb(2, 3, 4, 7, coorset, nano);
+	var blokLeft = drawrmb(7, 4, 5, 6, coorset, nano);
+	var blokInset1 = drawother(coorset, [6, 7, 2], nano, false);
+	var blokInset2 = drawother(coorset, [7, 4], nano, false);
+	var blokOutline = drawother(coorset, hexset, nano, true);
+
+	blokTop.attr({fill: "rgb(211,175,118)"});
+	blokRight.attr({fill: "rgb(191,155,98)"});
+	blokLeft.attr({fill: "rgb(171,135,78)"});
+	blokInset1.attr({stroke: "rgb(231,195,138)", "stroke-width": "2"});
+	blokInset2.attr({stroke: "rgb(231,195,138)", "stroke-width": "2"});
+	blokOutline.attr({stroke: "#444", "stroke-width": "2"});
+	
+	blok = nano.set();
+	blok.push(blokTop);
+	blok.push(blokRight);
+	blok.push(blokLeft);
+	blok.push(blokInset1);
+	blok.push(blokInset2);
+	blok.push(blokOutline);
+	
+	return blok;
+}
+
+/* Grid Functions */
+function changePerspective (isogrid, perspective, nano) {
 	switch(perspective) {
-		case 'iso45':
-		
-		case ''
+		case 'iso45': angle = {x: 45, y: 30, z: -30};
+		case 'iso': angle = {x: 45, y: 30, z: -30};
 	}
 	
 	removeGrid (isogrid);
 	isogrid = gridMatrix(grid, angle, scale, {x: 400, y: 100}, nano);
-}*/
+}
 
-function gridMatrix(grid, angle, scale, trans, nano) {
+function gridMatrix(grid, angle2, scale, trans, nano) {
+	
 	var tiles = $M([
 		[0],
 		[0],
 		[0],
 		[1]
-		])
+		]);
 	
 	var matrix = gridTiles (tiles, grid);
 	var board = new Array();
+	var gridSize = grid.c * grid.r + 1;
 	
-	for (var i = 0; i < grid.c * grid.r + 1; i++) {
+	for (var i = 0; i < gridSize; i++) {
+		progress(i, gridSize - 1);
+		
 		var slice = matrixSlice (matrix, i);
+		var angle = angleIso (angle2);
 
 		// Add each transformation together.
 		if (angle.x !== 0) {
@@ -113,10 +189,22 @@ function gridMatrix(grid, angle, scale, trans, nano) {
 		var arrayed = matrixArray (matrix, translated, i);
 		var tile = drawTiles(arrayed, angle, i, nano);
 		board[i] = tile;
-//		board = groupTiles(board, tile, i);
 	}
 	
 	return board;
+}
+
+function angleIso (angle2) {
+	var angle = new Object();
+	angle.x = degRads(angle2.x);
+	angle.y = Math.asin(Math.tan(degRads(angle2.y)));
+	angle.z = degRads(angle2.z);
+	
+/*	angle.x = degRads(45);
+	angle.y = Math.asin(Math.tan(degRads(30)));
+	angle.z = degRads(-30); */
+	
+	return angle;
 }
 
 // Creates a rectangular grid of coordinates.
@@ -137,10 +225,6 @@ function drawTiles (sq, angle, i, nano) {
 	return tile;
 }
 
-function groupTiles (board, tile, i) {
-	
-}
-
 function removeGrid (isogrid) {
 	for (var i = 0; i < grid.c * grid.r + 1; i++) {
 		isogrid[i].remove();
@@ -152,9 +236,6 @@ function buildSquare(size, angle, scale, trans, nano) {
 	var coors = new Array(4);
 
 	var square = matrixCoors (size);
-	
-	// Rotate the square; x is a vertical rotation, y is a horizontal rotation,
-	// and z rotates around the screen. Trust me, it makes sense.
 	
 	for (var i = 0; i < 4; i++) {
 		var slice = matrixSlice (square, i);
