3a1f7bec68a9a0bdb68c1370d0b0f0cb45d62a7d
Alex Trujillo <cryptoquick@gmail.com>
2009-08-07 16:07:36 -0600

    Yet another alternative approach to building an isometric gridwith raw SVG. Works in 5ms in WebKit. We'll see where this goes.

--- nb04/nanoblok.js
+++ nb04/nanoblok.js
@@ -6,10 +6,13 @@
  */
  
 // Universal constants
-var size = 35;
+var size = 50;
 var scale = {x: 1, y: 1};
 var grid = {r: 16, c: 16};
 var svgNS = 'http://www.w3.org/2000/svg';
+
+var angle = 45;
+var scaleY = 0.5;
 
 window.addEventListener('load', function () {
 	Init();
@@ -19,7 +22,8 @@ function Init () {
 	// Helps to know how fast things are going -- TEMP
 	var init0 = new Date();
 	
-	drawRects();
+	drawGrid();
+	testInput();
 	
 	var init1 = new Date();
 	debug('Program initialized in ' + (init1 - init0) + ' milliseconds.');
@@ -28,27 +32,73 @@ function Init () {
 /* Utility Functions */
 function debug(input) {
 	var debug = document.getElementById('debug');
-	debug.innerHTML = debug.innerHTML + '<br>' + input;
+	debug.textContent = debug.textContent + ' ' + input;
 }
 
-function drawRects() {
+function drawRects(group, coors) {
+	var rect = document.createElementNS(svgNS, 'rect');
+	rect.setAttributeNS(null, 'x', coors.x * size);
+	rect.setAttributeNS(null, 'y', coors.y * size);
+	rect.setAttributeNS(null, 'width', size);
+	rect.setAttributeNS(null, 'height', size);
+	
+	// matrix(scaleX, 0, transformX, 0, )
+	rect.setAttributeNS(null, 'transform', 'rotate(45)');
+	
+	group.appendChild(rect);
+}
+
+function drawGrid() {
 	var nano = document.getElementById('nano');
 	
 	var group = document.createElementNS(svgNS, 'g');
 	
-	var rect = document.createElementNS(svgNS, 'rect');
-	rect.setAttributeNS(null, 'x', 0);
-	rect.setAttributeNS(null, 'y', 0);
-	rect.setAttributeNS(null, 'width', 25);
-	rect.setAttributeNS(null, 'height', 50);
+	for (x = 0; x <= 15; x++) {
+		for (y = 0; y <= 15; y++) {
+			drawRects(group, {x: x, y: y});
+		}
+	}
 	
-	rect.setAttributeNS(null, 'transform', 'rotate(45)');
-//	rect.setAttributeNS(null, 'scale', 0.5);
-	
-	group.appendChild(rect);
 	group.setAttributeNS(null, 'stroke', 'black');
-	group.setAttributeNS(null, 'stroke-width', 2);
+	group.setAttributeNS(null, 'stroke-width', 1);
 	group.setAttributeNS(null, 'fill', 'white');
+	group.setAttributeNS(null, 'transform', 'scale(1, 0.5), translate(100, 100)');
+	group.setAttributeNS(null, 'id', 'rect1');
+//	group.setAttributeNS(null, 'transform', 'matrix(1, 0, 100, 0, 0.5, 100)');
 	
 	nano.appendChild(group);
+}
+
+function perspective(scaleY, angle) {
+	rect = document.getElementById('rect1');
+	rect.setAttributeNS(null, 'transform', 'scale(1, '+ scaleY +'), translate(100, 100)');
+	rect.firstChild.setAttributeNS(null, 'transform', 'rotate('+ angle +')');
+}
+
+function testInput() {
+	window.addEventListener('keydown', function(evt) {
+		// Input Handling
+		if (evt.type == 'keydown') {
+			// Some browsers support evt.charCode, some only evt.keyCode
+			if (evt.charCode) {
+				charCode = evt.charCode;
+			}
+			else {
+				charCode = evt.keyCode;
+			}
+		}
+		// Left arrow key
+		if (charCode == 37) {
+			angle++;
+			scaleY++;
+			perspective(scaleY / 20, angle);
+		}
+		// Right arrow key
+		if (charCode == 39) {
+			angle--;
+			scaleY--;
+			perspective(scaleY / 20, angle);
+		}
+	}, false);
+//	return isogrid;
 }
