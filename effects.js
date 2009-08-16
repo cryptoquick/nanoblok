8636df6258d6467af5385e85c3e908104808046b
Alex Trujillo <cryptoquick@gmail.com>
2009-08-15 18:01:34 -0600

    Forgot the new effects.js file.

---
+++ effects.js
@@ -0,0 +1,17 @@
+/* SVG Effects */
+
+var hoverlist = new Object();
+
+function tileHover (target, inout) {
+	var tile = document.getElementById(target.id);
+	loggit(target.id + ': ' + target.getAttribute('c') + ', ' + target.getAttribute('r'));
+	
+	if (inout == 'in') {
+		tile.setAttributeNS(null, 'fill', 'orange');
+		tile.setAttributeNS(null, 'stroke', '#aaa');
+	}
+	if (inout == 'out') {
+		tile.setAttributeNS(null, 'fill', '#ddd');
+		tile.setAttributeNS(null, 'stroke', '#aaa');
+	}
+}
