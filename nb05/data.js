032cd08634dd8c8109ed1fdc7d17758447e9502e
Alex Trujillo <cryptoquick@gmail.com>
2009-10-30 02:23:14 -0600

    MAJOR code cleanup... Almost finished.

--- nb05/data.js
+++ nb05/data.js
@@ -1,182 +1,133 @@
-//	Copyright 2009 Alex Trujillo
-//	Full source available here under the MIT License: http://code.google.com/p/nanoblok/
+/*
+ * Nanoblok (Experimental) - Web-Based Graphical Editor for Game Sprite Development
+ * http://code.google.com/p/nanoblok/
+ * Copyright (c) 2009 Alex Trujillo
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+ * 
+ * Summary for data.js:
+ * Handles data structures within the client, as well as requests to the server. Also computes common variables used throughout the program.
+ */
 	
-//	SUMMARY
-//	data.js handles data inside the program, such as the positions and IDs of blocks.
-
-// Is very similar to the PHP function of the same name
-function array_search (array, val, target) {
-	for (var i = 0; i < array.length; i++) {
-		if (array[i][target] == val) {
-			return i;
-		}
-	}
-	return false;
-}
-	
-// Field is short for Playfield, and each element contains 4 values; X, Y, Z, and color.
+// Field is short for Playfield, and each element contains 4 values; X, Y, Z, and color. This is strictly for serialization, and is in the order by which the user placed the blocks.
 var Field = new Array();
-//Field.length = 0;
 
+// GridField stores the positions of the grid tiles. These are set by gridCoors() in grid.js. Used by the other grid functions to render.
 var GridField = new Object();
 
 // Voxels are volumetric pixels, 3D coordinates. This keeps track of those in order to speed up spatial determinations, such as if a block is close by to another block.
 var Voxel = new Array();
 
-// The primary, generic function to intelligently record the position and state of all objects blockular in nature. 
-function blockRecord(blockID, position) {
-
-//	var blockAttributes = new Array(blockID, position, axis);
+// Many common variables used throughout the program.
+function computeCommonVars () {
+	// Viewport information from the browser.
+	var windowSize = {x: window.innerWidth, y: window.innerHeight};
 	
-//	currentPosition = {x: position.x + axis.x, y: position.y + axis.y, z: position.z};
+	// Change to a smaller display format if the window is too small. Not yet fully worked out.
+	if (windowSize.x < 725 || windowSize.y < 760)
+		var blockDims = 15; // For smaller screens
+	else {
+		var blockDims = 20; // Regular size
+	}
 	
-	Field[blockID] = {x: position.x, y: position.y, z: position.z};
+	// Size of blocks / tiles.
+	var blockSize = {
+		full: blockDims,
+		half: blockDims / 2,
+		third: blockDims / 2 + blockDims / 4,
+		quarter: blockDims / 4
+	}
 	
-	// Add new voxel position
-	VoxArray(position.x, position.y, position.z, blockID);
+	// Throughout the program, the c field is used for various measures. If c is not the same as r-- or vice versa--, problems will occur.
+	var gridDims = {c: 32, r: 32};
+	var gridSize = {
+		x: gridDims.c * blockSize.full,
+		y: gridDims.r * blockSize.quarter
+	};
 	
-	loggit('Block placed on the grid at ' + position.x + ', ' + position.y + ', ' + position.z);
+	// Center of the window.
+	var center = {x: windowSize.x / 2, y: windowSize.y / 2};
 	
-//	return {x: blockX, y: blockY, z: blockZ};
-}
-
-// Creates values within an associative arrray of established coordinates
-function VoxArray(x, y, z, value) {
-	if (Voxel[x] == null) {
+	// Grid edges.
+	var edges = {
+		left: center.x - gridSize.x / 2,
+		right: center.x + gridSize.x / 2,
+		top: windowSize.y - gridSize.y * 2
+	};
+	
+	// This should technically be how far away from the left and top of the screen that the left-most, top-most corner of grid is located.
+	var offset = {
+		x: (windowSize.x - gridSize.x) / 2,
+		y: windowSize.y - gridSize.y * 2
+	};
+	
+	// From Tango Project colors:
+	// http://tango.freedesktop.org/Tango_Icon_Theme_Guidelines
+	var defaultPalette = [
+		[164, 0, 0, 'red', null],
+		[211, 127, 4, 'orange', null],
+		[213, 184, 8, 'yellow', null],
+		[42, 197, 18, 'green', null],
+		[43, 84, 200, 'blue', null],
+		[147, 29, 199, 'purple', null],
+		[190, 67, 180, 'pink', null],
+		[201, 202, 188, 'white', null],
+		[55, 48, 51, 'black', null],
+		[255, 255, 255, 'transparent', null]
+	];
+	
+	// Various fields for selection states.
+	var selected = {
+		color: 0,
+		tool: "color",
+		area: {x: 0, y: 0, z: 0, l: 0, w: 0, h: 0}
+	};
+	
+	var layerOffset = {x: 0, y: 0, z: 0};
+	
+	// Updated as the marker is moved about the screen.
+	var markerPosition = {
+		x: 0,
+		y: 0,
+		z: 0
+	};
+	
+	// Put all the variables into one, common object.
+	var commonVars = {
+		blockDims: blockDims,
+		blockSize: blockSize,
+		gridDims: gridDims,
+		gridSize: gridSize,
+		windowSize: windowSize,
+		center: center,
+		edges: edges,
+		offset: offset,
+		palette: defaultPalette,
+		selected: selected,
+		layerOffset: layerOffset,
+		markerPosition: markerPosition
+	};
+	
+	// Initialize the voxel array.
+	for (var x = -1; x < gridDims.r + 1; x++) {
 		Voxel[x] = new Array();
-	}
-
-	if (Voxel[x][y] == null) {
-		Voxel[x][y] = new Array()
-	}
-
-	Voxel[x][y][z] = value;
-}
-
-// Find grid coordinates from the sequence number from the individual grid cell.
-function findGridXY(gridTarget) {
-	// Number ID of the grid element, longer for initial color names
-	var gridnum = parseInt(gridTarget.id.substr(7, 20));
-	
-	var gridCoordinates = new Object;
-	gridCoordinates.x = Math.floor(gridnum / 16);
-	gridCoordinates.y = gridnum % 16;
-	
-	return gridCoordinates;
-}
-
-function findBlockXY(gridTarget) {
-	// Number ID of the grid element, longer for initial color names
-	var gridnum = parseInt(gridTarget.substr(6, 20));
-	
-	var gridCoordinates = new Object;
-	gridCoordinates.x = Math.floor(gridnum / 16);
-	gridCoordinates.y = gridnum % 16;
-	
-	return gridCoordinates;
-}
-
-/*
-// Adds object to grid, plus some draw order logic (to keep visual perspective sane). It's also stupidly complex, and there might be a better way.
-function blockOrder (target, block) {
-	// Find grid coordinates (x,y)
-	var coor = findGridXY(target);
-
-	// If the block northeast of the current block is present, place behind (before it on the node hierarchy)
-	// However, before we do that, this variable will keep the second statement from being executed if they're both satisfied.
-	gojo = true; // Don't ask me about the name. I couldn't think of a better one. Great way to get your hands clean, though.
-	// If the block northeast of the current block is present, place behind (before it on the node hierarchy)
-	// Take note, the origin of the X axis is southeast, and the Y axis origin is southwest.
-	if (Voxel[(coor.x - 1)] !== undefined) {
-		if (Voxel[(coor.x - 1)][coor.y] !== undefined) {
-			if (Voxel[(coor.x - 1)][coor.y][0] !== undefined) {
-				var blockEast = Voxel[(coor.x - 1)][coor.y][0];
-				SVGRoot.insertBefore(block, document.getElementById(blockEast));
-				gojo = false;
-				loggit('Block placed at ' + coor.x + ', ' + coor.y + ', ' + 0 + ', behind ' + blockEast);
+		for (var y = -1; y < gridDims.r + 1; y++) {
+		Voxel[x][y] = new Array();
+			for (var z = -1; z < gridDims.c +1; z++) {
+				Voxel[x][y][z] = -1;
 			}
 		}
-	// If the block northwest of the current block is present, place behind (before it on the node hierarchy)
-	} else if (Voxel[coor.x] !== undefined) {
-		if (Voxel[coor.x][(coor.y + 1)] !== undefined) {
-			if (Voxel[coor.x][(coor.y + 1)][0] !== undefined && Voxel[coor.x][(coor.y + 1)][0] !== block.id && gojo == true) {
-				var blockNorth = Voxel[coor.x][(coor.y + 1)][0];
-				SVGRoot.insertBefore(block, document.getElementById(blockNorth));
-				gojo = false;
-				loggit(block.id + ' placed at ' + coor.x + ', ' + coor.y + ', ' + 0 + ', behind ' + blockNorth);
-			}
-		}
-	} else if (Voxel[coor.x - 1] !== undefined && Voxel[coor.x] !== undefined) {
-		if (Voxel[(coor.x - 1)][coor.y] !== undefined && Voxel[coor.x][(coor.y + 1)] !== undefined) {
-			if (Voxel[(coor.x - 1)][coor.y][0] !== undefined && Voxel[coor.x][(coor.y + 1)][0] !== undefined && Voxel[coor.x][(coor.y + 1)][0] !== block.id && gojo == true) {
-				var blockNorth = Voxel[coor.x][(coor.y + 1)][0];
-				SVGRoot.insertBefore(block, document.getElementById(blockNorth));
-				var blockEast = Voxel[(coor.x - 1)][coor.y][0];
-				SVGRoot.insertBefore(block, document.getElementById(blockEast));
-				loggit(block.id + ' placed at ' + coor.x + ', ' + coor.y + ', ' + 0 + ', behind ' + blockNorth + ' and ' + blockEast);
-			}
-		}
-	// If placed in order, append the block as normal
-	} else {
-		SVGRoot.appendChild(block);
-		loggit(block.id + ' placed at ' + coor.x + ', ' + coor.y + ', ' + 0 + '.');
 	}
-}
-*/
-
-function detection(target) {
 	
+	return commonVars;
 }
 
-// Bunch of terrible code that detects neighboring blocks based on a target position, and returns their neighbors.
-function neighbors(position) {
-	// Additional undefined neighbors logic for when the player places a block on the grid.
-	// This doesn't apply for z-axis voxels.
-	
-	neighbors = new Array();
-	
-//	neighbors[targetAxis] = target;
-	/*
-	for (i = 0; i < 5; i++) {
-		blockAxis ();
-	}
-	*/
-	
-	neighbors[0] = Voxel[position.x + 1][position.y][position.z];
-	neighbors[1] = Voxel[position.x - 1][position.y][position.z];
-	neighbors[2] = Voxel[position.x][position.y + 1][position.z];
-	neighbors[3] = Voxel[position.x][position.y - 1][position.z];
-	neighbors[4] = Voxel[position.x][position.y][position.z + 1];
-	neighbors[5] = Voxel[position.x][position.y][position.z - 1];
-	
-	// 0 = x+, 1 = x-, and so on.
-	loggit('x+: ' + neighbors[0] + ', x-: ' + neighbors[1] + ', y+: ' + neighbors[2] + ', y-: ' + neighbors[3] + ', z+: ' + neighbors[4] + ', z-: ' + neighbors[5] + '.');
-	
-	return neighbors;
-}
-/*
-function neighbors(target) {
-	target = target;
-	
-}
-*/
-/*
-Neighbors.prototype = {
-	// Axes which have a block nearby. Syntax is 0 = x+, 1 = x-, and so on.
-	this.neighbors = {'x0', 'x1', 'y0', 'y1', 'z0', 'z1'};
-	
-	findNearbyVoxels: function() {
-		this.neighbors.target;
-	}
-}
-*/
-
-// Field is the term for only the blocks on the board.
+// Serialization.
 function saveField () {
 	var fieldString = JSON.stringify(Field);
 	document.getElementById("saveFile").value = fieldString;
 }
 
+// Deserialization.
 function loadField () {
 	var fieldString = document.getElementById("saveFile").value;
 	Field = JSON.parse(fieldString);
