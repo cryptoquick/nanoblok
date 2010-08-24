601feabfeb9a3557aedf097dde2fe9199118a902
Firstname Lastname <firstname.lastname@example.net>
2010-08-23 22:40:14 -0600

    fix runtime bugs, add descriptive text and pseudocode comments for all methods.

--- nanoblok-edit/models.py
+++ nanoblok-edit/models.py
@@ -4,13 +4,11 @@ from google.appengine.ext import db
 class Sprite(db.Model):
 	user = db.UserProperty(auto_current_user_add=True)
 	name = db.StringProperty()
-	
-	
+
 class Voxel(db.Model):
-	sprite = db.ReferenceProperty(Sprite)
 	#Alex, I know you're using a slightly different definition of "voxel" in your main code, but I can't really think of a better term to use here.
+	sprite = db.ReferenceProperty(Sprite)
 	x = db.IntegerProperty()
 	y = db.IntegerProperty()
 	z = db.IntegerProperty()
 	color = db.IntegerProperty()
-	
