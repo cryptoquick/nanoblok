5b27349a8e5af9fa6b8b6538856e9aae9e1a1ca5
CryptoQuick <CryptoQuick@localhost>
2010-12-05 15:43:47 -0700

    Can now partially render a voxel field.

--- nbRender/render.js
+++ nbRender/render.js
@@ -1,28 +1,114 @@
+function colorOverhead () {
+	var pixArr = [];
+	
+	for (var x = 0; x < 32; x++) {
+		pixArr.push(new Array());
+		for (var y = 0; y < 32; y++) {
+			var color = {
+				r: SwatchField[Swatch[x][y][0]][3].r,
+				g: SwatchField[Swatch[x][y][0]][3].g,
+				b: SwatchField[Swatch[x][y][0]][3].b
+			}
+			
+			pixArr[x][y] = color;
+		}
+	}
+	
+	rectRender(pixArr);
+}
+
+function voxArr (dim) {
+	var voxels = [];	
+	
+	for (var x = 0; x < dim; x++) {
+		voxels[x] = [];
+		for (var y = 0; y < dim; y++) {
+			voxels[x][y] = [];
+			for (var z = 0; z < dim; z++) {
+				voxels[x][y][z] = null;
+			}
+		}
+	}
+	
+	return voxels;
+}
+
+function expand (field) {
+	var voxels = voxArr(32);
+//	console.log(field[0]);
+//	console.log(SwatchField[field[2][0]]);
+	for (var i = 0, ii = field.length; i < ii; i++) {
+	//	var vox = field[i];
+		var x = SwatchField[field[i][0]][0];
+		var y = SwatchField[field[i][1]][1];
+		var z = SwatchField[field[i][2]][2];
+		var color = SwatchField[field[i][3]][3];
+		
+		voxels[x][y][z] = color;
+	}
+	console.log(voxels[0][0][0]);
+	return voxels;
+}
+
+function overhead (field) {
+	var voxels = expand(field);
+	var pixels = [];
+	
+	for (var x = 0; x < 32; x++) {
+		pixels.push(new Array());
+		for (var y = 0; y < 32; y++) {
+			pixels[x][y] = {r: 255, g: 255, b: 255};
+			for (var z = 0; z < 32; z++) {
+				if (voxels[x] != null && voxels[x][y] != null && voxels[x][y][z] != null) {
+				//	console.log(pixels
+					
+				/*	var color = {
+						r: SwatchField[example1[0]][3].r,
+						g: SwatchField[example1[1]][3].g,
+						b: SwatchField[example1[2]][3].b
+					}*/
+				
+					pixels[x][y] = voxels[x][y][z];
+					break;
+				}
+			}
+		}
+	}
+	
+	rectRender(pixels);
+}
+
 // Pixel Width
-var pX = 2;
-var pY = 2;
+var pX = 6;
+var pY = 6;
 var dbg0 = 0;
 var dbg1 = 1;
 
-function RectRender() {
+function rectRender(pixArr) {
 	var canvas = document.getElementById('nbRender');
 	var ctx = canvas.getContext('2d');
 	
 	for (var x = 0; x < Math.floor(canvas.width / pX); x++) {
 		for (var y = 0; y < Math.floor(canvas.height / pY); y++) {
-			var xx = x % 32;
-			var yy = y % 32;
-			
-			ctx.fillStyle = "rgb(" +
-				SwatchField[Swatch[xx][yy][0]][3].r + "," +
-				SwatchField[Swatch[xx][yy][0]][3].g + "," +
-				SwatchField[Swatch[xx][yy][0]][3].b + ")";
-			ctx.fillRect(x * pX, y * pY, pX, pY);
+		//	var xx = x % 32;
+		//	var yy = y % 32;
+		
+			if (x < 32 && y < 32) {
+			/*	ctx.fillStyle = "rgb(" +
+					SwatchField[Swatch[x][y][0]][3].r + "," +
+					SwatchField[Swatch[x][y][0]][3].g + "," +
+					SwatchField[Swatch[x][y][0]][3].b + ")";*/
+				ctx.fillStyle = "rgb(" +
+					pixArr[x][y].r + "," +
+					pixArr[x][y].g + "," +
+					pixArr[x][y].b + ")";
+				ctx.fillRect(x * pX, y * pY, pX, pY);
+			}
 		}
 	}
 }
 
-function PixelRender () {
+function pixelRender () {
 	var canvas = document.getElementById('nbRender');
 	var ctx = canvas.getContext('2d');
 	var canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
