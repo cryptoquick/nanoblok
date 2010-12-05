ab5e5f426c81ce9c2f3a7064dd651d2c6a1b920a
CryptoQuick <CryptoQuick@localhost>
2010-12-05 08:57:11 -0700

    Started out nbRender with some semi-working code.

---
+++ nbRender/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+	Initialize();
+}, false);
+
+function Initialize () {
+	var date0 = new Date();
+	window.$C = new Common();
+	swatchInit();
+	Render();
+	var date1 = new Date();
+	console.log("Took " + (date1 - date0) + "ms to render.");
+}
