e85a64b1e2f0e006ed058fad12046076dc0d8bb2
CryptoQuick <CryptoQuick@localhost>
2011-10-23 23:44:54 -0600

    Just adding some stuff for way off in the future.

---
+++ nb10/blok/assembly/Assembly.js
@@ -0,0 +1,1 @@
+// An assembly is a set of parts with constraints that describe relationships between each other.
---
+++ nb10/blok/assembly/Part.js
@@ -0,0 +1,2 @@
+// A part represents a single object, and may consist of multiple 32vx graphical representations.
+// For example, parts can be used as part of an animation, or to represent small portions of more complex systems.
