45ce35c6238f077dcc115c877c0e2146e998c01f
CryptoQuick <CryptoQuick@localhost>
2012-07-15 17:15:15 -0600

    Committing some boilerplate Canvas code for posterity, but there's a more efficient way to do this...

--- nb11/app/canvas.js
+++ nb11/app/canvas.js
@@ -29,11 +29,37 @@ function (color) {
 	};
 
 	// Render
-	canvas.draw = function (data) {
+	canvas.simpleDraw = function (voxels) {
 		var width = canvas.els[0].width,
 			height = canvas.els[0].height,
 			index = 0,
-			data = canvas.img[0].data;
+			data = canvas.img[0].data,
+			x = 0, y = 0,
+			color = [];
+
+		for (var v = 0, vv = voxels.length; v < vv; v++) {
+			index = x + y * width;
+		}
+
+		for (y = 0; y < height; y++) {
+			for (x = 0; x < width; x++) {
+				index = x + y * width;
+				color = pixels[index];
+				index = index * 4;
+				if (color) {
+					data[index + 0] = color[0];
+					data[index + 1] = color[1];
+					data[index + 2] = color[2];
+					data[index + 3] = 255;
+				}
+				else {
+					data[index + 0] = 255;
+					data[index + 1] = 255;
+					data[index + 2] = 255;
+					data[index + 3] = 0;
+				}
+			}
+		}
 	};
 
 	return canvas;
