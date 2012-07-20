043a703e99c480f0c8dc4a439e8bdac24e27155a
CryptoQuick <CryptoQuick@localhost>
2012-07-20 16:56:53 -0600

    Tried one fill route, going to go for another.

--- nb11/app/render.js
+++ nb11/app/render.js
@@ -12,14 +12,6 @@ function (canvas, matrix) {
 	// Render
 	render.full = function (data) {
 		// canvas.simpleDraw(data, 16, 0, 0.5);
-	/*	canvas.drawPoly([
-			250, 0,
-			500, 125,
-			500, 375,
-			250, 500,
-			0  , 375,
-			0  , 125
-		]); */
 		canvas.drawPoly([
 			250, 0,
 			500, 125,
@@ -28,6 +20,41 @@ function (canvas, matrix) {
 			0  , 375,
 			0  , 125
 		]);
+	/*	canvas.drawPoly([
+			0, 375,
+			250, 250,
+			0, 125
+		]);
+		canvas.drawPoly([
+			250, 0,
+			250, 250,
+			500, 125
+		]); // :(
+		canvas.drawPoly([
+			500, 125,
+			250, 250,
+			500, 375
+		]);  // :)
+		canvas.drawPoly([
+			500, 375,
+			250, 250,
+			250, 500
+		]); // :( 
+		canvas.drawPoly([
+			250, 500,
+			250, 250,
+			0, 375
+		]);   // :T 
+		canvas.drawPoly([
+			0, 375,
+			250, 250,
+			0, 125
+		]); // :(
+		canvas.drawPoly([
+			0, 125,
+			250, 250,
+			250, 0
+		]);*/
 	};
 
 	render.init();
