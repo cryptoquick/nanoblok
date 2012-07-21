1ed3b27c4d93bb69912fda33cbf28a486c7f2aca
CryptoQuick <CryptoQuick@localhost>
2012-07-20 22:19:37 -0600

    Bugfix, cleanup, testing, optimization; BCTO.

--- nb11/app/canvas.js
+++ nb11/app/canvas.js
@@ -115,25 +115,30 @@ function (colors, utils) {
 
 	// Digital Differential Analyzer, DDA. Ported to JS by CQ.
 	canvas.dda = function (x0, y0, x1, y1) {
-		var Dx = x1 - x0,
-			Dy = y1 - y0,
-			steps = 0,
-			k = 0,
-			xincr = 0.0, yincr = 0.0,
-			x = x0, y = y0;
-			ax = Math.abs(Dx), ay = Math.abs(Dy),
+		var dx = x1 - x0, // Distance
+			dy = y1 - y0,
+			steps = 0, k = 0,	// Steps / Iterations
+			xincr = 0.0, yincr = 0.0, // Increment per step
+			x = x0, y = y0; // Start
+			ax = Math.abs(dx), ay = Math.abs(dy), // |distance|
 			width = canvas.els[0].width;
 
-		if (ax > ay)
+		// Get the most steps
+	/*	if (ax >= ay)
 			steps = ax;
 		else
-			steps = ay;
+			steps = ay;	*/
 
-		xincr = Dx / steps;
-		yincr = Dy / steps;
+		steps = ay; // Not sure when this 'fix' will become a bug...
 
-		// console.log(steps, x, y, xincr, yincr, width);
+		// Determine step increments
+		xincr = dx / steps;
+		yincr = dy / steps;
 
+		// Debug
+		console.log(steps, xincr, yincr, width);
+
+		// Loop~
 		for (k = 0; k < steps; k++) {
 			x += xincr;
 			y += yincr;
@@ -172,30 +177,23 @@ function (colors, utils) {
 		ctx.fill();
 
 		// Draw indices computed by line().
-		canvas.drawIndices(4);
+		canvas.drawIndices(2);
 	}
 
 	canvas.drawIndices = function (pxsize) {
-		var lineBuffer = canvas.lineBuffer,
-			line = [],
-			img = canvas.ctx[0].getImageData(0, 0, canvas.els[0].width, canvas.els[0].height),
+		var img = canvas.ctx[0].getImageData(0, 0, canvas.els[0].width, canvas.els[0].height),
 			data = img.data,
 			index = 0,
 			bufferindex = 0,
 			width = canvas.els[0].width,
-			ylast = 0,
 			x = 0, y = 0,
-			x0 = width + 1, x1 = -1,
-			bufflen = lineBuffer.length;
-	
-		// console.log(lineBuffer[406080]);
+			bufflen = canvas.lineBuffer.length;
 
 		while (bufflen--) {
-			for (y = 0; y < pxsize; y++) {
-				for (x = 0; x < pxsize; x++) {
-					bufferindex = lineBuffer[bufflen];
-					index = y * width + bufferindex + x;
-					index *= 4;
+			bufferindex = canvas.lineBuffer[bufflen];
+			for (y = -pxsize; y < pxsize; y++) {
+				for (x = -pxsize; x < pxsize; x++) {
+					index = (y * width + bufferindex + x) * 4;
 					data[index] 	= 255;
 					data[index + 1] = 0;
 					data[index + 2] = 0;
