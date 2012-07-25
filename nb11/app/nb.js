0937b68d8592852f33173be54d2972b8e962c0c2
CryptoQuick <CryptoQuick@localhost>
2012-07-24 20:25:12 -0600

    Great input system, some early work on 3D transformation.

--- nb11/app/nb.js
+++ nb11/app/nb.js
@@ -5,13 +5,56 @@ define([
 ],
 function (render, utils, EXAMPLES) {
 	// Global nanoblok object.
-	var nb = {};
+	var nb = {},
+	x = 0, y = 0, rx = 0, ry = 0, rz = 0;
 
 	nb.init = function () {
 		utils.benchmark(function () {
 			render.blok();
-			// render.test2();
 		}, 1);
+
+		window.addEventListener('keydown', function(evt) {
+			var rd = 0.1,
+				td = 10.0,
+				actions = {};
+
+			actions[65] = function () {
+				ry -= rd;
+			}
+			actions[68] = function () {
+				ry += rd;
+			}
+			actions[87] = function () {
+				rx -= rd;
+			}
+			actions[83] = function () {
+				rx += rd;
+			}
+			actions[81] = function () {
+				rz -= rd;
+			}
+			actions[69] = function () {
+				rz += rd;
+			}
+			actions[37] = function () {
+				x -= td;
+			}
+			actions[39] = function () {
+				x += td;
+			}
+			actions[38] = function () {
+				y -= td;
+			}
+			actions[40] = function () {
+				y += td;
+			}
+
+			if (actions[evt.keyCode]) {
+				evt.preventDefault();
+				render.test2(x, y, rx, ry, rz);
+				console.log('keypress', evt.keyCode, x, y, rx, ry, rz);
+			}
+		}, false);
 	};
 
 	nb.init();
